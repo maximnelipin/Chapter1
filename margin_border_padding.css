46383fde62833825567798a20dcacfcad12a970e
Maxim Nelipin <nelipin.maxim@yandex.ru>
2015-05-10 16:51:09 +0300

    Изучаем отсутпы поля границы

---
+++ margin_border_padding.css
@@ -0,0 +1,15 @@
+body {
+background: linen;
+width: 500px;
+margin: 200px auto;
+}
+div {
+border: 5px solid maroon;
+text-align: center;
+margin-left:10px;
+}
+p {
+border: 2px dashed blue;
+padding: 10px;
+margin:10px;
+}
