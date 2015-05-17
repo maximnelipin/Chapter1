3b8043aff1fa579c67e50589775c1ece98a3debb
Maxim Nelipin <nelipin.maxim@yandex.ru>
2015-05-17 21:44:31 +0300

    Немного стиля + обработка клика

--- First_jQuery.js
+++ First_jQuery.js
@@ -1,15 +1,7 @@
-body {
-background: linen;
-width: 500px;
-margin: 200px auto;
-}
-div {
-border: 5px solid maroon;
-text-align: center;
-margin-left:10px;
-}
-p {
-border: 2px dashed blue;
-padding: 10px;
-margin:10px;
-}
+var main = function () {
+	"use strict";
+	$(".comment-input button").on("click", function (event) {
+	console.log("Hello, World!");
+	});
+};
+$(document).ready(main);
