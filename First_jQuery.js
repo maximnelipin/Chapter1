2107f86d2bc497fbb1bb246fc44624310d54eb86
Maxim Nelipin <nelipin.maxim@yandex.ru>
2015-05-17 22:09:55 +0300

    Добавили интерактивности
    Чистка поля после добавлния комментария, лавный комментарий, отправка
    комментария после нажатия Enter, Сделали код более компактным за счёт
    выделения обработчика в новую функцию

--- First_jQuery.js
+++ First_jQuery.js
@@ -1,7 +1,23 @@
 var main = function () {
 	"use strict";
-	$(".comment-input button").on("click", function (event) {
-	console.log("Hello, World!");
+	var addCommentFromInputBox = function () { //функция добаления комментрия
+		var $new_comment;
+		if($(".comment-input input").val()!==""){//Если поле непустое		
+			$new_comment=$("<p>").text($(".comment-input input").val()); //создаем новый комментрий
+			$new_comment.hide(); //скрыли
+			$(".comments").append($new_comment); //добавили на страницу
+			$new_comment.fadeIn(); //плавно вывели
+			$(".comment-input input").val(""); //очмстили поле ввода
+			}
+	};
+	$(".comment-input button").on("click", function (event) { //по нажатию на кнопку !!!! Обрабатываем кнопку BUTTON
+		addCommentFromInputBox();
+	});
+	$(".comment-input input").on("keypress", function (event) {
+		var $new_comment;
+		if (event.keyCode === 13) { //по нажатию на Enter !!!! Обрабатываем поле ввода INPUT
+			addCommentFromInputBox();
+		}
 	});
 };
 $(document).ready(main);
